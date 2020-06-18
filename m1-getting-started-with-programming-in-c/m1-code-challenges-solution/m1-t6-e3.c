c7d264a7d635e3c386c53e8c647ef29f34bb7667
abhishek516 <67084897+abhishek516@users.noreply.github.com>
2020-06-18 09:46:45 +0530

    Update m1-t6-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t6-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t6-e3.c
@@ -2,10 +2,10 @@
 
 int main() {
 
-    char name[10] = "India";
+    char name[10] = "chin";
     char dollar = '$';
-    float gdp = 2.2;
-    int year = 2015;
+    float gdp = 3.6;
+    int year = 2020;
 
     printf("As economic reforms picked up the pace, %s's GDP grew five-fold to reach US%c%.1f trillion in %d (as per IMF estimates).", name, dollar, gdp, year);
 
