5a90c5e80b1f3a66e958eaf8694139efa5f7bc05
abhishek516 <67084897+abhishek516@users.noreply.github.com>
2020-06-19 18:03:37 +0530

    Update m1-t9-e4.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t9-e4.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t9-e4.c
@@ -5,29 +5,29 @@ int main() {
     // WAP to find out the grade of a student based on marks secured in the exam.
     // Code challenge solution
 
-    char grade = ‘X’;
+    char grade = â€˜Xâ€™;
     int marks;
     puts("Please enter your marks: ");
     scanf("%d", &marks);
 
-    if (marks >= 450 && marks <=500) {
+    if (marks >= 500 && marks <=600) {
         grade = 'A';
-    } else if (marks >= 400 && marks < 450) {
+    } else if (marks >= 450 && marks <500) {
         grade = 'B';
-    } else if (marks >= 350 && marks < 400) {
+    } else if (marks >= 400 && marks < 450) {
         grade = 'C';
-    } else if (marks >= 250 && marks < 350) {
+    } else if (marks >= 300 && marks < 400) {
         grade = 'D';
-    } else if (marks >= 150 && marks < 250) {
+    } else if (marks >= 180 && marks < 300) {
         grade = 'E';
-    } else if (marks >= 0 && marks < 150) {
+    } else if (marks >= 0 && marks < 180) {
         grade = 'F';
     } else {    // For negative value or a value greater than 500
         puts("Invalid input.");
     }
     
-/* If the marks entered by the user is a valid mark between 0 and 500 then ‘grade’ would not be equal to ‘X’ i.e. he has been assigned a valid grade.  */
-    if (grade != ‘X’)
+/* If the marks entered by the user is a valid mark between 0 and 500 then â€˜gradeâ€™ would not be equal to â€˜Xâ€™ i.e. he has been assigned a valid grade.  */
+    if (grade != â€˜Xâ€™)
         printf("\nYour grade is: %c", grade);
 
     return 0;
